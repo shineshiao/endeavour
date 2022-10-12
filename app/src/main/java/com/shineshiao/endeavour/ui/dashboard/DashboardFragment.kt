07bc080442186d92715207f0f60eb13cbac8c17b
Thach.Nguyen <thach.nguyenngoc@positivethinking.tech>
2022-10-12 23:59:19 +1030

    * add klint format

--- app/src/main/java/com/shineshiao/endeavour/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/shineshiao/endeavour/ui/dashboard/DashboardFragment.kt
@@ -6,9 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.shineshiao.endeavour.R
 import com.shineshiao.endeavour.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -24,7 +22,7 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
@@ -32,9 +30,12 @@ class DashboardFragment : Fragment() {
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        dashboardViewModel.text.observe(
+            viewLifecycleOwner,
+            {
+                textView.text = it
+            }
+        )
         return root
     }
 
